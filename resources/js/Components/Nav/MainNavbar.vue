abbaf04f79878d15c07018eb61878f29c29caa05
Miqayel <tech_dev@amlincubator.com>
2025-03-29 19:03:29 +0400

    init

--- resources/js/Components/Nav/MainNavbar.vue
+++ resources/js/Components/Nav/MainNavbar.vue
@@ -3,12 +3,13 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="flex">
+    <div class="flex text-gray text-sm tracking-widest uppercase">
+        <p class=" ">Home</p>
         <Link
-            :href="route('home')"
-            class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+            :href="route('home')">
             Menu
         </Link>
+        
         <Link
             v-if="$page.props.auth.user"
             :href="route('dashboard')"
