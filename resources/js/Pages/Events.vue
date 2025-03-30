a191650b225d8f3964e868d38649a335838c0b87
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-03-30 18:16:18 +0400

    event api

--- resources/js/Pages/Events.vue
+++ resources/js/Pages/Events.vue
@@ -11,6 +11,20 @@ defineProps({
 
 <template>
     <GuestLayout>
-        {{ events }}
+        <div class="flex flex-col max-w-screen-sm mx-auto">
+            <h1 class="text-2xl font-bold">Events</h1>
+            <div class="flex flex-col gap-y-10">
+                <div v-for="event in events.data" :key="event.id" class="mb-2">
+                    <div class="relative">
+                        <img :src="event.poster" :alt="event.title" class="w-full h-full object-cover object-center" />
+                        <div class="absolute bottom-0 w-full h-52 bg-gradient-to-t from-black to-transparent">
+                            <h3 class="text-2xl text-center pt-20">{{ event.title }} </h3>
+                            <p class="text-center text-gray-300">{{ event.location }}</p>
+                            <p class="text-center"><b>{{ event.start_date }}</b> <span>{{ event.start_time }}</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </GuestLayout>
 </template>
