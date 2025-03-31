2fea13318e828e1085d82bd12ad37ec94395d1a1
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-01 00:15:36 +0400

    event page

--- resources/js/Pages/Events.vue
+++ resources/js/Pages/Events.vue
@@ -16,11 +16,11 @@ defineProps({
             <div class="flex flex-col gap-y-10">
                 <div v-for="event in events.data" :key="event.id" class="mb-2 h-[600px]">
                     <div class="relative h-full" :style="{ backgroundImage: `url(${event.poster})`}">
-                        <div class="absolute inset-0 backdrop-blur-md z-0"></div>
+                        <div class="absolute inset-0 backdrop-blur-md z-0 brightness-50"></div>
                         <img :src="event.poster" :alt="event.title"
                              class="absolute w-full h-full object-contain object-center z-10" />
-                        <div class="absolute z-20 bottom-0 w-full h-52 bg-gradient-to-t from-black to-transparent">
-                            <h3 class="text-2xl text-center pt-32">{{ event.title }} </h3>
+                        <div class="absolute z-20 bottom-0 w-full h-48 bg-gradient-to-t from-black to-transparent">
+                            <h3 class="text-2xl text-center pt-20">{{ event.title }} </h3>
                             <p class="text-center text-gray-300">{{ event.location }}</p>
                             <p class="text-center"><b>{{ event.start_date }}</b> <span>{{ event.start_time }}</span></p>
                         </div>
