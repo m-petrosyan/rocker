b4eb947eedb292355c2e1eb36c5d8ebf2f1c598f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-03 00:26:48 +0400

    map

--- resources/js/Components/Map.vue
+++ resources/js/Components/Map.vue
@@ -1,33 +1,25 @@
 <template>
     <GoogleMap
-        v-if="center.lat && center.lng"
+        v-if="props.cordinates.latitude && props.cordinates.longitude"
         api-key="AIzaSyCovr1rcKSduU9SLpe_IX-EzuF-_sVVAlY"
-        :center="center"
+        :center="{ lat: props.cordinates.latitude, lng: props.cordinates.longitude }"
         :zoom="15"
         :styles="darkTheme"
-        :map-options="mapOptions"
         style="width: 100%; height: 500px"
     >
-        <Marker :options="{ position: center }" />
+        <Marker :options="{ position: { lat: props.cordinates.latitude, lng: props.cordinates.longitude } }" />
     </GoogleMap>
 </template>
 
 <script setup>
 import { GoogleMap, Marker } from 'vue3-google-map';
 
-
 const props = defineProps({
     cordinates: {
         type: Object,
-        required: true,
-        default: () => ({ latitude: 40.689247, longitude: -74.044502 }) //
+        required: true
     }
 });
-
-const center = {
-    lat: 40.689247,
-    lng: -74.044502
-};
 
 const darkTheme = [
     { elementType: 'geometry', stylers: [{ color: '#212121' }] },
@@ -39,10 +31,4 @@ const darkTheme = [
     { featureType: 'water', elementType: 'geometry', stylers: [{ color: '#000000' }] },
     { featureType: 'poi', elementType: 'geometry', stylers: [{ color: '#1a1a1a' }] }
 ];
-
-const mapOptions = {
-    styles: darkTheme, // Include styles here too for compatibility
-    mapTypeControl: false, // Removes Map/Satellite toggle
-    streetViewControl: false // Removes Pegman (Street View icon)
-};
 </script>
