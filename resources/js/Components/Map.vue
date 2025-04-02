ca7897d1b4fc60b5b6dee4d1bcba7e2f461a59a9
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-02 23:53:44 +0400

    map

--- resources/js/Components/Map.vue
+++ resources/js/Components/Map.vue
@@ -8,15 +8,42 @@ defineProps({
     }
 });
 
+// Define dark theme styles (example from Snazzy Maps or custom)
+const darkThemeStyles = [
+    { elementType: 'geometry', stylers: [{ color: '#212121' }] },
+    { elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
+    { elementType: 'labels.text.fill', stylers: [{ color: '#757575' }] },
+    { elementType: 'labels.text.stroke', stylers: [{ color: '#212121' }] },
+    {
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [{ color: '#757575' }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#2c2c2c' }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#000000' }]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{ color: '#424242' }]
+    }
+];
 </script>
 
 <template>
     <GoogleMap
-        styles="dark"
         api-key="AIzaSyCovr1rcKSduU9SLpe_IX-EzuF-_sVVAlY"
         style="width: 100%; height: 500px"
-        :center="center"
+        :center="cordinates"
         :zoom="15"
+        :styles="darkThemeStyles"
     >
         <Marker :options="{ position: cordinates }" />
     </GoogleMap>
