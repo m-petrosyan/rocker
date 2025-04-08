a2af2091e614696bd6f4a2676f9d3bc9ba1b9df5
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-08 04:30:39 +0400

    update profile layout and add contact information

--- resources/js/Pages/Profile/Profile.vue
+++ resources/js/Pages/Profile/Profile.vue
@@ -33,18 +33,37 @@ defineProps({
         <!--        </template>-->
 
         <div>
-            <div class="mx-auto bg-graydark py-2 sm:px-6 lg:px-8">
-                <div class="mx-auto w-fit text-center">
-                    <div class="relative">
+            <div
+                class="relative mx-auto mt-36 bg-graydark py-2 sm:px-6 lg:px-8"
+            >
+                <div
+                    class="absolute left-1/2 top-[-100px] w-fit -translate-x-1/2 text-center"
+                >
+                    <div class="relative mx-auto w-fit">
                         <img
                             src="/images/user.avif"
                             alt="Profile Picture"
-                            class="mx-auto mt-6 h-32 w-32 rounded-full object-cover"
+                            class="mx-auto mt-6 h-32 w-32 rounded-full border-4 border-white object-cover"
                         />
                         <QrGenerate v-if="url" :url />
                     </div>
-                    <div class="text-gray-900 p-6">
+                    <div class="text-gray-900 p-6 font-bold">
                         {{ user.name }}
+                    </div>
+                </div>
+                <div class="mt-32 text-center">
+                    <small class="mx-auto block w-2/6 text-sm text-gray">
+                        "Creative professional based in Kyiv, passionate about
+                        building meaningful projects and bringing ideas to life.
+                        Focused on quality, collaboration, and continuous
+                        growth."
+                    </small>
+                    <div class="mt-5 flex flex-col">
+                        <a href="mailto:example@gmail.com">example@gmail.com</a>
+                        <a
+                            href="https://www.facebook.com/groups/286409629705239/"
+                            >facebook.com/groups/286409629705239/</a
+                        >
                     </div>
                 </div>
                 <ProfileActions v-if="owner" class="mx-auto w-1/2" />
