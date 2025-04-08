cb46d2568cdbb87be089269b887797b584708213
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-09 02:03:22 +0400

    testing multi markers

--- resources/js/Pages/Events/Events.vue
+++ resources/js/Pages/Events/Events.vue
@@ -17,11 +17,27 @@ defineProps({
         <template #header> Events</template>
         <MegaMaps
             :markers="[
-                { latitude: 40.188067, longitude: 44.5139577 },
-                { latitude: 40.1800023, longitude: 44.5160956 },
-                { latitude: 40.1895131, longitude: 44.521072 },
-                { latitude: 40.1800023, longitude: 44.5160956 },
-                { latitude: 40.1737167, longitude: 44.516279399999995 },
+                { latitude: 40.188067, longitude: 44.5139577, info: 'Tonlab' },
+                {
+                    latitude: 40.1800023,
+                    longitude: 44.5160956,
+                    info: 'The venue',
+                },
+                {
+                    latitude: 40.1895131,
+                    longitude: 44.521072,
+                    info: 'Stop club',
+                },
+                {
+                    latitude: 40.1800023,
+                    longitude: 44.5160956,
+                    info: 'Tonlab2',
+                },
+                {
+                    latitude: 40.1737167,
+                    longitude: 44.516279399999995,
+                    info: 'Hardrock',
+                },
             ]"
         />
         <!--        <MultiSelect class="w-48" />-->
