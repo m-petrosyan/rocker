b353e95cb4a17b2b7fc9e29bed03a1bc7c0114bc
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-08 22:22:44 +0400

    fix create

--- app/Http/Controllers/Profile/EventController.php
+++ app/Http/Controllers/Profile/EventController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Requests\Event\EventCreateRequest;
+use App\Services\EventService;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class EventController
 {
+    public function __construct(protected EventService $eventService)
+    {
+    }
+
     public function create(): Response
     {
         return Inertia::render('Profile/Events/EventCreateEdit');
@@ -19,61 +22,14 @@ class EventController
 
     public function store(EventCreateRequest $request): RedirectResponse
     {
-        $response = null;
+        $response = $this->eventService->store($request);
 
-        try {
-            $validated = $request->validated();
-            $http = Http::timeout(10)
-                ->throw()
-                ->retry(3, 1000)
-                ->withHeaders(['Accept' => 'application/json']);
+        dd($response->body());
+        dd($response?->status());
 
-            $payload = [
-                'rocker[username]' => auth()->user()->username,
-                'rocker[role]' => 'admin',
-                'title' => $validated['title'],
-                'content' => $validated['content'],
-                'type' => $validated['type'],
-                'country' => $validated['country'],
-                'location' => $validated['location'],
-                'coordinates[latitude]' => $validated['cordinates']['latitude'] ?? null,
-                'coordinates[longitude]' => $validated['cordinates']['longitude'] ?? null,
-                'genre' => $validated['genre'],
-                'price' => $validated['price'] ?? null,
-                'start_date' => $validated['start_date'],
-                'start_time' => $validated['start_time'],
-                'link' => $validated['link'] ?? null,
-                'ticket' => $validated['ticket'] ?? null,
-            ];
-
-            if ($request->hasFile('poster_file') && $request->file('poster_file')->isValid()) {
-                $file = $request->file('poster_file');
-                $http->attach(
-                    'poster_file',
-                    file_get_contents($file->path()),
-                    $file->getClientOriginalName()
-                );
-            }
-
-            $response = $http->post('http://bot.rocker.loc/api/event', $payload);
-
-            return redirect()
-                ->back()
-                ->with('success', 'Событие успешно создано');
-        } catch (\Exception $e) {
-            Log::error(
-                $e->getMessage().' Не удалось создать событие: '.$e->getTraceAsString(),
-                [
-                    'status' => $response?->status(),
-                    'body' => $response?->body(),
-                ]
-            );
-
-            return redirect()
-                ->back()
-                ->with('error', 'Ошибка при создании события: '.$e->getMessage())
-                ->withInput();
-        }
+        return redirect()
+            ->back()
+            ->with('success', 'Событие успешно создано');
     }
 
     public function update()
--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class EventService
 {
@@ -22,5 +23,59 @@ class EventService
         }
 
         return $data;
+    }
+
+    public function store($request)
+    {
+        $response = null;
+
+        try {
+            $validated = $request->validated();
+            $http = Http::timeout(10)
+                ->throw()
+                ->withHeaders(['Accept' => 'application/json']);
+
+            $payload = [
+                'rocker[username]' => auth()->user()->username,
+                'rocker[role]' => 'admin',
+                'title' => $validated['title'],
+                'content' => $validated['content'],
+                'type' => $validated['type'],
+                'country' => $validated['country'],
+                'location' => $validated['location'],
+                'coordinates[latitude]' => $validated['cordinates']['latitude'] ?? null,
+                'coordinates[longitude]' => $validated['cordinates']['longitude'] ?? null,
+                'genre' => $validated['genre'],
+                'price' => $validated['price'] ?? null,
+                'start_date' => $validated['start_date'],
+                'start_time' => $validated['start_time'],
+                'link' => $validated['link'] ?? null,
+                'ticket' => $validated['ticket'] ?? null,
+            ];
+
+            if ($request->hasFile('poster_file') && $request->file('poster_file')->isValid()) {
+                $file = $request->file('poster_file');
+                $http->attach(
+                    'poster_file',
+                    file_get_contents($file->path()),
+                    $file->getClientOriginalName()
+                );
+            }
+
+            return $http->post('http://bot.rocker.loc/api/event', $payload);
+        } catch (\Exception $e) {
+            Log::error(
+                $e->getMessage().' Не удалось создать событие: '.$e->getTraceAsString(),
+                [
+                    'status' => $response?->status(),
+                    'body' => $response?->body(),
+                ]
+            );
+
+            return redirect()
+                ->back()
+                ->with('error', 'Ошибка при создании события: '.$e->getMessage())
+                ->withInput();
+        }
     }
 }
