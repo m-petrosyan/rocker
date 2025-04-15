9eefb8f1d5927222e48217c744548544a9c94b54
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-15 14:59:07 +0400

    profile img, gallery

--- resources/js/Pages/Gallery/Gallery.vue
+++ resources/js/Pages/Gallery/Gallery.vue
@@ -13,8 +13,8 @@ defineProps({
 <template>
     <GuestLayout :meta="{title: gallery.title}">
         <h1 class="text-center">{{ gallery.title }}</h1>
-        <div class="flex mt-5">
-            <div class="w-1/3">
+        <div class="flex flex-col md:flex-row mt-5 gap-y-6">
+            <div class="md:w-1/3">
                 <NavLink :href="route('profile.show', gallery.user.username)"
                          class="text-center flex flex-col items-center gap-y-4 p-2">
                     <div class="h-16 w-16 rounded-full overflow-hidden">
@@ -24,10 +24,10 @@ defineProps({
                     <p>{{ gallery.user.name }}</p>
                 </NavLink>
             </div>
-            <div class="w-2/3 flex flex-col gap-y-4 bg-graydark p-4 rounded-lg text-pretty">
+            <div class="md:w-2/3 flex flex-col gap-y-4 bg-graydark p-4 rounded-lg text-pretty">
                 <p>{{ gallery.description }}</p>
                 <p class="bg-graydark2 w-fit px-2 rounded-sm">{{ gallery.date }}</p>
-                <div class="flex gap-x-2">
+                <div class="flex  flex-wrap gap-x-2">
                     <div v-for="band of gallery.bands" class="bg-red px-1 rounded-sm">
                         <div class="bg-red ">{{ band.name }}</div>
                     </div>
