4dccf51f486dfac3c7e077ed314279e9ae0e2e28
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-17 20:50:04 +0400

    fix route

--- resources/js/Layouts/AuthLayouth.vue
+++ resources/js/Layouts/AuthLayouth.vue
@@ -3,6 +3,7 @@ import MainNavbar from '@/Components/Nav/MainNavbar.vue';
 import PwaNavbar from '@/Components/Nav/PwaNavbar.vue';
 import { Head, usePage } from '@inertiajs/vue3';
 import ErrorMessages from '@/Components/Messages/ErrorMessages.vue';
+import { onMounted, ref } from 'vue';
 
 defineProps({
     title: {
@@ -13,15 +14,22 @@ defineProps({
 });
 
 const page = usePage();
+
+const isPWA = ref(false);
+
+onMounted(() => {
+    isPWA.value = window.matchMedia('(display-mode: standalone)').matches ||
+        window.navigator.standalone === true;
+});
 </script>
 
 <template>
     <Head :title />
-    <MainNavbar v-if="page.url !== '/' && !$isPWA" />
+    <MainNavbar v-if="page.url !== '/' && !isPWA" />
     <main class="mt-20 sm:mx-auto w-full sm:w-8/12 md:w-4/12 md:max-w-[500px] rounded-lg bg-graydark p-6">
         <ErrorMessages :messages="$page.props.errors" />
         <slot />
     </main>
     <slot name="underslot" />
-    <PwaNavbar v-if="$isPWA" />
+    <PwaNavbar v-if="isPWA" />
 </template>
