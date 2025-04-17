c157789da4d75bd3366cfba7c399bbcb9d7d741e
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-04-18 02:07:59 +0400

    alert

--- resources/js/Components/Nav/MainNavbar.vue
+++ resources/js/Components/Nav/MainNavbar.vue
@@ -65,8 +65,8 @@ onBeforeUnmount(() => {
         </div>
     </nav>
     <Teleport to="body">
-        <nav class="absolute md:hidden w-full h-lvh top-0 left-0 "
-             :class="{'bg-blackTransparent z-50' : showBurger}">
+        <nav class="absolute md:hidden w-full top-0 left-0 "
+             :class="{'bg-blackTransparent z-50 h-lvh' : showBurger}">
             <div class=" flex justify-end">
                 <button @click="showBurger = !showBurger" data-collapse-toggle="navbar-hamburger" type="button"
                         class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
