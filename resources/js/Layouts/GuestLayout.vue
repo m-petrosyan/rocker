0cd3c6e36e2ce96cc41ca76b284a6bc8d24bf9fc
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-05-06 01:18:39 +0400

    add links

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -31,7 +31,8 @@ onMounted(() => {
 <template>
     <Head :title="meta?.title ?? defaultTitle">
         <meta name="description" :content="meta?.description ??  defaultDescription" />
-        <meta name="og:description" :content="meta?.description?.replace(/<[^>]*>/g, '') ?? defaultDescription" />
+        <meta name="og:description"
+              :content="(meta?.description?.replace(/<[^>]*>/g, '') ?? defaultDescription).slice(0, 160)" />
         <meta name="og:title" :content="meta?.title ?? defaultTitle" />
         <meta name="keywords" :content="meta?.keywords ?? defaultKeywords" />
         <meta name="og:image" :content="meta?.image ?? defaultImg" />
