4cfc47631f69a4561f43d5f06888ee82959e38e3
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-05-06 01:57:26 +0400

    add links

--- resources/js/Pages/Band/Band.vue
+++ resources/js/Pages/Band/Band.vue
@@ -27,15 +27,18 @@ const getHostname = (url) => {
 <template>
     <GuestLayout
         :meta="{title: band.name ,image:band.logo?.large , description: band.info, keywords: band.name}">
-        <div class="h-96 relative">
-            <div v-if="band.links.length" class="absolute right-0 w-fit top-0 bg-opacity-20 bg-graydark p-3">
+        <div class="lg:h-96 h-64  relative">
+            <div v-if="band.links.length"
+                 class="absolute flex md:flex-col gap-x-5 right-0 w-fit top-0 bg-opacity-20 bg-graydark p-3">
                 <a v-for="link in band.links" :key="link.id" :href="link.url"
                    target="_blank">{{ getHostname(link.url)
                     }}</a>
             </div>
-            <img :src="band.cover?.large" class="w-full h-full object-cover" :alt="band.name">
-            <img :src="band.logo?.thumb" class="w-48 absolute left-0 bottom-0" :alt="band.name">
-            <div class="absolute h-1/6 left-1/2 w-fit -translate-x-1/2 text-center bottom-0 bg-graydark p-5">
+            <img :src="band.cover?.large" class="w-full h-full lg:object-cover object-contain" :alt="band.name">
+            <img :src="band.logo?.thumb"
+                 class="z-10 md:w-48  w-3/12 absolute lg:bottom-0 lg:-translate-x-0 lg:left-0 bottom-5 left-1/2 w-fit  -translate-x-1/2"
+                 :alt="band.name">
+            <div class="absolute h-1/6 left-1/2 lg:w-fit w-full -translate-x-1/2 text-center bottom-0 bg-graydark p-5">
                 <h1>{{ band.name }}</h1>
             </div>
             <SocialShare class="absolute right-0 bottom-0 bg-opacity-20 bg-graydark" :title="band.name" :url />
@@ -46,7 +49,7 @@ const getHostname = (url) => {
             </h3>
             <GenresTags class="mx-auto w-fit" :genres="band.genres" />
         </div>
-        <div class="md:w-5/6 mx-auto mt-8" v-html="band.info" />
+        <div class="md:w-5/6 mx-auto mt-8 md:p-0 p-3" v-html="band.info" />
         <h3 class="text-center mt-10">Galleries</h3>
         <GalleryWrapper :galleries="band.galleries" user />
     </GuestLayout>
