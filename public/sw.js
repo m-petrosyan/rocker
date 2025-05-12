0a0bca0fcb74deaa85ebb90faf39214c87beec6e
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-05-12 20:28:40 +0400

    pwd preloader

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,11 @@
 self.addEventListener('fetch', event => {
+    const isInertiaRequest = event.request.headers.get('X-Inertia');
+
+    if (isInertiaRequest) {
+        // Let browser handle Inertia navigation without interference
+        return;
+    }
+
     event.respondWith(
         fetch(event.request).then(response => {
             if (new URL(event.request.url).pathname === '/') {
