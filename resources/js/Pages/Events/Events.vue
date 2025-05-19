606349f7630b805334752eeefed1ac4a6212b0ba
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-05-20 00:14:40 +0400

    add views

--- resources/js/Pages/Events/Events.vue
+++ resources/js/Pages/Events/Events.vue
@@ -1,9 +1,13 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import EventWrapper from '@/Components/Wrappers/EventWrapper.vue';
+import MegaMaps from '@/Components/Maps/MegaMaps.vue';
 
 defineProps({
     events: {
+        type: Object
+    },
+    auth: {
         type: Object
     }
 });
@@ -12,32 +16,34 @@ defineProps({
 <template>
     <GuestLayout :meta="{ title: 'Events' }">
         <template #header> Events</template>
-        <!--        <MegaMaps-->
-        <!--            :markers="[-->
-        <!--                { latitude: 40.188067, longitude: 44.5139577, info: 'Tonlab' },-->
-        <!--                {-->
-        <!--                    latitude: 40.1800023,-->
-        <!--                    longitude: 44.5160956,-->
-        <!--                    info: 'The venue',-->
-        <!--                },-->
-        <!--                {-->
-        <!--                    latitude: 40.1895131,-->
-        <!--                    longitude: 44.521072,-->
-        <!--                    info: 'Stop club',-->
-        <!--                },-->
-        <!--                {-->
-        <!--                    latitude: 40.1800023,-->
-        <!--                    longitude: 44.5160956,-->
-        <!--                    info: 'Tonlab2',-->
-        <!--                },-->
-        <!--                {-->
-        <!--                    latitude: 40.1737167,-->
-        <!--                    longitude: 44.516279399999995,-->
-        <!--                    info: 'Hardrock',-->
-        <!--                },-->
-        <!--            ]"-->
-        <!--        />-->
-        <!--        <MultiSelect class="w-48" />-->
+
+        <MegaMaps
+            v-if="auth?.isAdmin"
+            :markers="[
+                        { latitude: 40.188067, longitude: 44.5139577, info: 'Tonlab' },
+                        {
+                            latitude: 40.1800023,
+                            longitude: 44.5160956,
+                            info: 'The venue',
+                        },
+                        {
+                            latitude: 40.1895131,
+                            longitude: 44.521072,
+                            info: 'Stop club',
+                        },
+                        {
+                            latitude: 40.1800023,
+                            longitude: 44.5160956,
+                            info: 'Tonlab2',
+                        },
+                        {
+                            latitude: 40.1737167,
+                            longitude: 44.516279399999995,
+                            info: 'Hardrock',
+                        },
+                    ]"
+        />
+        <MultiSelect class="w-48" />
         <EventWrapper :events add />
     </GuestLayout>
 </template>
