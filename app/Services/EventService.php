c032c13d922b917a4631a33650f6720d80699a31
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-06-07 17:18:54 +0400

    event ge add

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Traits\ComponentServiceTrait;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class EventService
@@ -59,6 +60,8 @@ class EventService
             ]);
 
             $this->addSyncBand($event, $attributes);
+
+            Cache::forget('events');
 
             return $data;
         } catch (\Exception $e) {
