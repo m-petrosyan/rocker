05fd7f751c776c65376012bb96cc987cf7b0a172
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-07-02 14:44:22 +0400

    bot migration

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 
 use App\Notifications\NewCreationNotification;
 use App\Traits\ComponentServiceTrait;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
 
@@ -63,7 +62,7 @@ class EventService
 
             $this->addSyncBand($event, $attributes);
 
-            Cache::forget('events');
+//            Cache::forget('events');
 
             Notification::route('mail', config('mail.admin.address'))
                 ->notify(new NewCreationNotification($event));
