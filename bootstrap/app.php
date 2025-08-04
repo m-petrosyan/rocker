9ac27dcd85de9c5258d87952dce88fa30e361ecf
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-04 23:47:18 +0400

    add sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Sentry\Laravel\Integration;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 
@@ -29,5 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('backup:run')->daily()->at('20:00');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        if (!app()->environment('local')) {
+            Integration::handles($exceptions);
+        }
     })->create();
