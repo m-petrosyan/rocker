05f8f842ee39b23c33ddc712a54cb731cdebe017
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-10 01:24:46 +0400

    add notification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,7 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('backup:run')->daily()->at('20:00');
         $schedule->command('backup:clean')->daily()->at('21:00');
-        $schedule->command('disk:check')->daily()->at('21:00');
+        $schedule->command('disk:check')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         if (!app()->environment('local')) {
