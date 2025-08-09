b1584a160241b0b84dfbc8aeadca816b8e31b44d
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-10 01:14:32 +0400

    ad d notification

--- app/Console/Commands/CheckDiskSpaceCommand.php
+++ app/Console/Commands/CheckDiskSpaceCommand.php
@@ -2,25 +2,25 @@
 
 namespace App\Console\Commands;
 
-use DefStudio\Telegraph\Facades\Telegraph;
+use App\Models\UserBot;
 use Illuminate\Console\Command;
 
-class CheckDiskSpace extends Command
+class CheckDiskSpaceCommand extends Command
 {
     protected $signature = 'disk:check';
     protected $description = 'Проверяет заполненность диска и отправляет уведомление в Telegram, если занято более 90%';
 
-    public function handle()
+    public function handle(): void
     {
         $drive = '/';
         $usedSpace = (1 - disk_free_space($drive) / disk_total_space($drive)) * 100;
 
         $prcent = 5;
-        dump($usedSpace);
+
         if ($usedSpace >= $prcent) {
-            Telegraph::message("⚠️ Диск заполнен на {$usedSpace}%!")
-                ->chatId(config('telegraph.webhook.chat_id'))
-                ->send();
+            $chat = UserBot::where('chat_id', config('telegraph.webhook.chat_id'))->firstOrFail();
+
+            $chat->message("⚠️ Диск заполнен на {$usedSpace}%!")->send();
 
             $this->info('Уведомление отправлено в Telegram.');
         } else {
