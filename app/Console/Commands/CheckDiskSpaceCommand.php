d7bccea73dffeab03322b8c6e5457ac6a6d802ce
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-10 01:23:46 +0400

    add notification

--- app/Console/Commands/CheckDiskSpaceCommand.php
+++ app/Console/Commands/CheckDiskSpaceCommand.php
@@ -15,7 +15,7 @@ class CheckDiskSpaceCommand extends Command
         $drive = '/';
         $usedSpace = (1 - disk_free_space($drive) / disk_total_space($drive)) * 100;
 
-        $prcent = 90;
+        $prcent = 5;
 
         if ($usedSpace >= $prcent) {
             $chat = UserBot::where('chat_id', config('telegraph.webhook.chat_id'))->firstOrFail();
@@ -25,7 +25,7 @@ class CheckDiskSpaceCommand extends Command
             $this->info('Уведомление отправлено в Telegram.');
         } else {
             $usedSpace = round((1 - disk_free_space($drive) / disk_total_space($drive)) * 100);
-            $this->info("Диск заполнен менее чем на  $usedSpace %.");
+            $this->info("Диск заполнен менее чем на {$usedSpace}%.");
         }
     }
 }
