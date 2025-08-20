2f624469a188278d21520eb7b4f39b7321ac2b23
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-20 07:26:54 +0400

    canonical

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,7 @@
     <link rel="shortcut icon" href="/favicon.ico">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}" />
+    <link rel="canonical" href="{{ url()->current() }}" />
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
     @routes
     @vite([
