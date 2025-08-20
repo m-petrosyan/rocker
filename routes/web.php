878e105cbfde24d5445b2caa28c06cb17ddb3f8c
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-20 07:59:56 +0400

    add 404 page

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,6 @@ Route::prefix('auth')->group(function () {
     Route::get('/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
 });
 
+Route::fallback(function () {
+    return Inertia::render('404');
+});
