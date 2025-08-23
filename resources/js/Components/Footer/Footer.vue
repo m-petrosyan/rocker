092fc18ad834f4e60f65d7eb906a1c0ed452f15f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-23 16:52:25 +0400

    change footer

--- resources/js/Components/Footer/Footer.vue
+++ resources/js/Components/Footer/Footer.vue
@@ -8,10 +8,11 @@ import BotIcon from '@/Components/Icons/BotIcon.vue';
     <footer>
         <div class="flex justify-center md:justify-end pr-3 pb-1">
             <div class="relative flex items-center flex-col md:flex-row justify-between w-full pl-2 text-graydarker">
-                <p class="text-center"> © {{ new Date().getFullYear() }} rocker.am. Some rights reserved. |
+                <p class="order-1 text-center"> © {{ new Date().getFullYear() }} rocker.am. Some rights reserved. |
                     <a href="mailto:info@rocker.am">info@rocker.am</a></p>
 
-                <div class="absolute left-1/2 -translate-x-1/2 bottom-14 md:bottom-0 flex gap-x-2">
+                <div
+                    class="order-0 md:order-1 py-2 md:py-0 md:absolute left-1/2  bottom-14 md:bottom-0 flex gap-x-2">
                     <a href="https://www.instagram.com/rocker._.am/"
                        target="_blank">
                         <InstagramIcon />
@@ -21,7 +22,7 @@ import BotIcon from '@/Components/Icons/BotIcon.vue';
                         <BotIcon />
                     </a>
                 </div>
-                <a href="https://t.me/mpetrosyan1" target="_blank">
+                <a class="order-3" href="https://t.me/mpetrosyan1" target="_blank">
                     <small>Developed by Miqayel Petrosyan</small>
                 </a>
             </div>
