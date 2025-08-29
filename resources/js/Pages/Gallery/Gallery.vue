173af5d9b9a584e427b84e519ebdb81c7fe0079f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-29 19:42:46 +0400

    add meta

--- resources/js/Pages/Gallery/Gallery.vue
+++ resources/js/Pages/Gallery/Gallery.vue
@@ -20,11 +20,14 @@ const props = defineProps({
 const venueName = computed(() => {
     return props.gallery.venue?.location ?? props.gallery.venue?.name;
 });
+
+const description = 'by ' + props.gallery.user.name + '. ' + (props.gallery.description || 'Discover Armenian rock and metal bands, upcoming concerts, announcements, and photo galleries. Stay updated with the Armenian music scene on Rocker.am.');
+
 </script>
 
 <template>
     <GuestLayout
-        :meta="{title: gallery.title+' – Armenian Rock/Metal Music, Concerts & Albums' ,image:gallery.cover_img.large , description: 'by '+gallery.user.name + gallery.description+'. Discover Armenian rock and metal bands, upcoming concerts, announcements, and photo galleries. Stay updated with the Armenian music scene on Rocker.am.', author:gallery.user.name,keywords: gallery.bands.map(band => band.name).join(',')}">
+        :meta="{title: gallery.title+' – Armenian Rock/Metal Music, Concerts & Albums' ,image:gallery.cover_img.large , description, author:gallery.user.name,keywords: gallery.bands.map(band => band.name).join(',')}">
         <h2 class="text-center">{{ gallery.title }}</h2>
         <div class="flex flex-col-reverse md:flex-row mt-5 gap-y-6">
             <div class="md:w-1/3">
