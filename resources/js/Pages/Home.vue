828bd070d0e2e731d880d4189bfc6d7ba3b46d47
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-29 18:54:38 +0400

    add meta

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -28,8 +28,13 @@ console.log(Ziggy);
 </script>
 
 <template>
-    <GuestLayout :meta="{title: 'Home - Armenian Rock & Metal Bands, Concerts & Galleries'}">
+    <GuestLayout :meta="{title: 'Armenian Rock & Metal Bands, Concerts & Galleries'}">
         <template #h1>Armenian Rock & Metal Bands, Concerts & Galleries</template>
+        <p class="text-pretty text-gray mb-6">Welcome to Rocker.am – your portal for Armenian rock and metal bands.
+            Discover
+            upcoming
+            concerts, explore
+            photo galleries from live performances, and stay updated with the Armenian music scene.</p>
         <EventWrapper :events="events.data" more title="Events" />
         <BandWrapper :bands="bands.data" more title="Bands" />
         <GalleryWrapper :galleries="galleries.data" more title="Galleries" />
