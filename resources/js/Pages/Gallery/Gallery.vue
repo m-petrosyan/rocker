7d71f9ca963d2b5e870e90cf4769512745b6cb7f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-08-29 19:45:19 +0400

    add meta

--- resources/js/Pages/Gallery/Gallery.vue
+++ resources/js/Pages/Gallery/Gallery.vue
@@ -28,6 +28,7 @@ const description = 'by ' + props.gallery.user.name + '. ' + (props.gallery.desc
 <template>
     <GuestLayout
         :meta="{title: gallery.title+' – Armenian Rock/Metal Music, Concerts & Albums' ,image:gallery.cover_img.large , description, author:gallery.user.name,keywords: gallery.bands.map(band => band.name).join(',')}">
+        <h1>{{ gallery.title }}</h1>
         <h2 class="text-center">{{ gallery.title }}</h2>
         <div class="flex flex-col-reverse md:flex-row mt-5 gap-y-6">
             <div class="md:w-1/3">
