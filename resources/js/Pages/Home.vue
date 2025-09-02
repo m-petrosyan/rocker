a5ab15e46188c6e3c5d45da34126f0e0474023a5
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-09-02 20:28:47 +0400

    fixing queries

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -27,16 +27,18 @@ defineProps({
 
 <template>
     <GuestLayout :meta="{title: 'Armenian Rock & Metal Bands, Concerts & Galleries'}">
+
         <template #h1>Armenian Rock & Metal Bands, Concerts, Galleries</template>
         <p class="text-pretty text-gray mb-6">Welcome to Rocker.am – your portal for Armenian rock and metal bands.
             Discover
             upcoming
             concerts, explore
             photo galleries from live performances, and stay updated with the Armenian music scene.</p>
+
         <EventWrapper :events="events.data" more title="Events" />
         <BandWrapper :bands="bands.data" more title="Bands" />
         <GalleryWrapper :galleries="galleries.data" more title="Galleries" />
-        <section class="mb-8">
+        <section class="my-20">
             <h2 class="text-pretty text-gray mb-6 text-[16px] font-medium">The Armenian Rock & Metal Scene</h2>
             <p class="text-gray ">
                 Armenia has a unique and vibrant rock and metal culture that blends local
