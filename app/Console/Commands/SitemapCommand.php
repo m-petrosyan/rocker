47172330ed1216cec642e74879cfb04c0d3e9280
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-09-08 15:22:42 +0400

    past events

--- app/Console/Commands/SitemapCommand.php
+++ app/Console/Commands/SitemapCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Event;
 use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 
@@ -28,6 +30,8 @@ class SitemapCommand extends Command
     public function handle(): void
     {
         \Log::info('Sitemap generation started.', ['time' => now()]);
+
+        // Сначала генерируем базовый sitemap
         SitemapGenerator::create(config('app.url'))
             ->setConcurrency(10)
             ->hasCrawled(function (Url $url) {
@@ -40,6 +44,35 @@ class SitemapCommand extends Command
 
                 return $url;
             })
-            ->writeToFile(public_path('sitemap.xml'));
+            ->writeToFile(public_path('sitemap-temp.xml'));
+
+        // Загружаем уже сгенерированные URL
+        $sitemap = Sitemap::createFromFile(public_path('sitemap-temp.xml'));
+
+        // Добавляем все события
+        $allEvents = Event::all();
+        foreach ($allEvents as $event) {
+            $sitemap->add(
+                Url::create("/events/{$event->id}")
+                    ->setLastModificationDate($event->updated_at)
+            );
+        }
+
+        // Добавляем страницы пагинации для past events
+        $perPage = 52;
+        $totalPastPages = ceil(Event::where('start_date', '<', now())->count() / $perPage);
+        for ($i = 1; $i <= $totalPastPages; $i++) {
+            $sitemap->add(Url::create("/events/past?page={$i}"));
+        }
+
+        // Записываем финальный sitemap
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        // Удаляем временный файл
+        if (file_exists(public_path('sitemap-temp.xml'))) {
+            unlink(public_path('sitemap-temp.xml'));
+        }
+
+        \Log::info('Sitemap generation finished.', ['time' => now()]);
     }
 }
