d89ef5948bad7dd57a2c6f1c4383c4eb0738304d
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-09-11 06:15:08 +0400

    seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#000000">
     <meta name="publisher" content="Rocker.am">
-    <meta name="apple-mobile-web-app-title" content="Rocker.am">
+    <meta name="theme-color" content="#000000">
+    <meta name="apple-mobile-web-app-title" content="rocker.am">
     <link rel="manifest" href="/manifest.webmanifest">
     <link rel="apple-touch-icon" href="/icons/icon-192.png">
     <link rel="icon" type="image/png" href="/favicon.png">
+    {{--    <link rel="canonical" href="{{ url()->current() }}" />--}}
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
     <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@type": "WebSite",
-            "url": "{{ url('/') }}",
-            "name": "Rocker"
-        }
+    @verbatim
+            {
+              "@context": "https://schema.org",
+      "@type": "WebSite",
+      "url": "{{ url('/') }}",
+      "name": "Rocker"
+    }
+        @endverbatim
     </script>
     @routes
     @vite([
@@ -23,3 +29,7 @@
     ])
     @inertiaHead
 </head>
+<body class="font-sans antialiased bg-gray-900 text-white">
+@inertia
+</body>
+</html>
