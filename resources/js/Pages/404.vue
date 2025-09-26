c434e89076647305c03c1b1d673e19d42aa7a644
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-09-26 21:55:37 +0400

    404 page

--- resources/js/Pages/404.vue
+++ resources/js/Pages/404.vue
@@ -4,9 +4,14 @@ import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 <template>
     <GuestLayout :meta="{title: '404 page not found'}">
-        <template #header><span class="hidden">404 page not found </span></template>
+        <template #header><span>404 page not found </span></template>
         <div class="flex justify-center items-center">
-            <img src="/images/404.png" alt="404 page not found" />
+            <img src="/images/404.gif" alt="404 page not found" />
+        </div>
+        <div class="text-center">
+            <h2>We lost this page</h2>
+            <p> We searched high and low but couldn’t find what you’re looking for. Let’s find a better place for you to
+                go.</p>
         </div>
     </GuestLayout>
 </template>
