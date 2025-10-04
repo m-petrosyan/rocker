3f926a755fce2b20a8d95e5726e464afb73699ad
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-04 13:04:16 +0400

    fixing select

--- app/Http/Requests/Band/BandCreateRequest.php
+++ app/Http/Requests/Band/BandCreateRequest.php
@@ -27,7 +27,7 @@ class BandCreateRequest extends FormRequest
             'cover_file' => ['required', 'image', 'mimes:webp,jpeg,jpg,webp,png', 'max:15000'],
             'logo_file' => ['required', 'mimes:jpeg,jpg,webp,png,svg', 'max:4000'],
             'images' => ['nullable', 'array', 'max:7'],
-            'images.*' => ['image', 'mimes:webp,jpeg,png,jpg', 'max:25000'],
+            'images.*' => ['image', 'mimes:webp,jpeg,png,jpg', 'max:50000'],
             'info' => ['required', 'string', 'min:100', 'max:4000'],
             'genres' => ['required', 'array', 'max:255'],
             'links' => ['nullable', 'array'],
