88c54a22fdbeb3d9ac7d8fab490f0e0e651a325f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-07 12:44:01 +0400

    bands title

--- resources/js/Pages/Band/Band.vue
+++ resources/js/Pages/Band/Band.vue
@@ -9,64 +9,64 @@ import EventWrapper from '@/Components/Wrappers/EventWrapper.vue';
 import BandAlbums from '@/Components/Forms/BandAlbums.vue';
 
 defineProps({
-    band: {
-        type: Object,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    events: {
-        type: Object,
-        required: true
-    }
+  band: {
+    type: Object,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  events: {
+    type: Object,
+    required: true
+  }
 });
 </script>
 
 <template>
-    <GuestLayout
-        :meta="{title: band.name+' – Armenian Rock/Metal Band' ,image: band.logo?.svg ?? band.logo?.large , description: band.info, keywords: band.name+ ' '+ band.genres.map(genre =>  genre.name).join(',')+' Armenian band ' }">
-        <div class="lg:h-96 h-64  relative">
-            <div v-if="band.links.length"
-                 class="absolute flex md:flex-col gap-x-5 right-0 w-fit top-0 bg-opacity-20 bg-graydark p-3">
-                <a v-for="link in band.links" :key="link.id" :href="link.url"
-                   target="_blank">{{ getHostname(link.url)
-                    }}</a>
-            </div>
-            <img
-                :src="band.cover?.large"
-                class="w-full h-full object-cover"
-                :alt="band.name"
-                :style="band.cover_position ? `object-position: ${band.cover_position.x}% ${band.cover_position.y}%` : ''"
-            />
-            <img :src="band.logo?.svg ?? band.logo?.thumb"
-                 class="z-10 lg:w-48 md:w-32 w-2/12 absolute lg:bottom-0 lg:-translate-x-0 lg:left-0 bottom-5 left-1/2  -translate-x-1/2"
-                 :alt="band.name">
-            <div class="absolute h-1/6 left-1/2 lg:w-fit w-full -translate-x-1/2 text-center bottom-0 bg-graydark p-5">
-                <h1>{{ band.name + ' Armenian ' + band.genres.map(genre => genre.name).join(' Band from Armenia')
-                    }}</h1>
-                <h2>{{ band.name }}</h2>
-            </div>
-            <SocialShare class="absolute right-0 bottom-0 bg-opacity-20 bg-graydark" :title="band.name" :url />
-        </div>
-        <div :class="band.name.length > 40 ? 'md:mt-20 sm:mt-32 mt-56' : 'mt-5'" class="flex flex-col gap-y-4 ">
-            <h4 class="text-center mt-5">
-                Genres
-            </h4>
-            <GenresTags class="mx-auto w-fit" :genres="band.genres" />
-        </div>
-        <ImageWrapper classes="flex gap-4" :images="band.images_url" :title="band.title" :url />
-        <div class="md:w-5/6 mx-auto mt-8 md:p-0 p-3 whitespace-break-spaces" v-html="band.info" />
-        <BandAlbums v-if="band?.albums" :albums="band.albums" />
-        <EventWrapper v-if="events?.data.length" class="mt-20" :events="events.data" title="Upcoming events" />
-        <GalleryWrapper v-if="band?.galleries.length" :galleries="band.galleries" title="Galleries" user />
-    </GuestLayout>
+  <GuestLayout
+    :meta="{title: band.name+' – '+band.genres.map(genre =>  genre.name).join(', ')+' Armenian band' ,image: band.logo?.svg ?? band.logo?.large , description: band.info, keywords: band.name+ ' '+ band.genres.map(genre =>  genre.name).join(',')+' Armenian band ' }">
+    <div class="lg:h-96 h-64  relative">
+      <div v-if="band.links.length"
+           class="absolute flex md:flex-col gap-x-5 right-0 w-fit top-0 bg-opacity-20 bg-graydark p-3">
+        <a v-for="link in band.links" :key="link.id" :href="link.url"
+           target="_blank">{{ getHostname(link.url)
+          }}</a>
+      </div>
+      <img
+        :src="band.cover?.large"
+        class="w-full h-full object-cover"
+        :alt="band.name"
+        :style="band.cover_position ? `object-position: ${band.cover_position.x}% ${band.cover_position.y}%` : ''"
+      />
+      <img :src="band.logo?.svg ?? band.logo?.thumb"
+           class="z-10 lg:w-48 md:w-32 w-2/12 absolute lg:bottom-0 lg:-translate-x-0 lg:left-0 bottom-5 left-1/2  -translate-x-1/2"
+           :alt="band.name">
+      <div class="absolute h-1/6 left-1/2 lg:w-fit w-full -translate-x-1/2 text-center bottom-0 bg-graydark p-5">
+        <h1>{{ band.name + ' Armenian ' + band.genres.map(genre => genre.name).join(' Band from Armenia')
+          }}</h1>
+        <h2>{{ band.name }}</h2>
+      </div>
+      <SocialShare class="absolute right-0 bottom-0 bg-opacity-20 bg-graydark" :title="band.name" :url />
+    </div>
+    <div :class="band.name.length > 40 ? 'md:mt-20 sm:mt-32 mt-56' : 'mt-5'" class="flex flex-col gap-y-4 ">
+      <h4 class="text-center mt-5">
+        Genres
+      </h4>
+      <GenresTags class="mx-auto w-fit" :genres="band.genres" />
+    </div>
+    <ImageWrapper classes="flex gap-4" :images="band.images_url" :title="band.title" :url />
+    <div class="md:w-5/6 mx-auto mt-8 md:p-0 p-3 whitespace-break-spaces" v-html="band.info" />
+    <BandAlbums v-if="band?.albums" :albums="band.albums" />
+    <EventWrapper v-if="events?.data.length" class="mt-20" :events="events.data" title="Upcoming events" />
+    <GalleryWrapper v-if="band?.galleries.length" :galleries="band.galleries" title="Galleries" user />
+  </GuestLayout>
 </template>
 
 <style>
 .ql-video {
-    width: 100%;
-    height: 550px;
+  width: 100%;
+  height: 550px;
 }
 </style>
