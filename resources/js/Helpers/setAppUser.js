7eb932c0e47384247adb625fb250d27e8c69b752
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-16 18:20:04 +0400

    fix login

--- resources/js/Helpers/setAppUser.js
+++ resources/js/Helpers/setAppUser.js
@@ -1,29 +1,28 @@
 import axios from 'axios';
-import { router } from '@inertiajs/vue3';
 
 export function useTelegramAuth() {
-    const initData = isWebApp(true);
-    if (initData) {
-        const params = new URLSearchParams(initData);
-        const userParam = params.get('user');
-        if (userParam) {
-            const user = JSON.parse(decodeURIComponent(userParam));
-            axios.post('/telegram/auth', { id: user.id })
-                .then(res => {
-                    if (res.data?.redirect) {
-                        const url = new URL(res.data.redirect, window.location.origin);
-                        router.visit(url.pathname + url.search);
-                    }
-                });
-        }
+  const initData = isWebApp(true);
+  if (initData) {
+    const params = new URLSearchParams(initData);
+    const userParam = params.get('user');
+    if (userParam) {
+      const user = JSON.parse(decodeURIComponent(userParam));
+      axios.post('/telegram/auth', { id: user.id })
+        .then(res => {
+          // if (res.data?.redirect) {
+          //     const url = new URL(res.data.redirect, window.location.origin);
+          //     router.visit(url.pathname + url.search);
+          // }
+        });
     }
+  }
 }
 
 export function isWebApp(returnInitData = false) {
-    if (typeof window === 'undefined') {
-        return returnInitData ? null : false;
-    }
+  if (typeof window === 'undefined') {
+    return returnInitData ? null : false;
+  }
 
-    const initData = window?.Telegram?.WebApp?.initData;
-    return returnInitData ? initData : !!initData;
+  const initData = window?.Telegram?.WebApp?.initData;
+  return returnInitData ? initData : !!initData;
 }
