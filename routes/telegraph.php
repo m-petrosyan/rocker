fdb8f1612e9b3d16761acc0aa33e4a1f68089a90
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-17 16:11:53 +0400

    user create fixing

--- routes/telegraph.php
+++ routes/telegraph.php
@@ -2,16 +2,16 @@
 
 use App\Models\UserBot;
 
-\Illuminate\Support\Facades\Log::info('78787878asdasdasd');
+//\Illuminate\Support\Facades\Log::info('78787878asdasdasd');
 
 $userId = data_get(request()->input('callback_query'), 'from.id') ?? data_get(request()->input('message'), 'from.id');
-\Illuminate\Support\Facades\Log::info('$userId', ['userId' => $userId]);
+//\Illuminate\Support\Facades\Log::info('$userId', ['userId' => $userId]);
 if ($userId) {
     $userBot = UserBot::query()->where('chat_id', $userId)->first();
-    \Illuminate\Support\Facades\Log::info('$userBot', [$userBot]);
+//    \Illuminate\Support\Facades\Log::info('$userBot', [$userBot]);
     if ($userBot) {
-        \Illuminate\Support\Facades\Log::info('$userBot', [$userBot->user?->id]);
+//        \Illuminate\Support\Facades\Log::info('$userBot', [$userBot->user?->id]);
         auth()->loginUsingId($userBot->user?->id);
-        \Illuminate\Support\Facades\Log::info('auth()->id()', ['auth_id' => auth()->id()]);
+//        \Illuminate\Support\Facades\Log::info('auth()->id()', ['auth_id' => auth()->id()]);
     }
 }
