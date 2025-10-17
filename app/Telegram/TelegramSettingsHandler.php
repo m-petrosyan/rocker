68a94e47f77263bfd2d4ddfe3a23683b2b53c6cf
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-17 16:08:03 +0400

    user create fixing

--- app/Telegram/TelegramSettingsHandler.php
+++ app/Telegram/TelegramSettingsHandler.php
@@ -34,7 +34,7 @@ trait TelegramSettingsHandler
         ];
         Log::info('countries');
         foreach (trans('settings.countries') as $icon => $value) {
-            Log::info('auth37', [auth()->user]);
+            Log::info('auth37', [auth()->user()]);
             $checked = auth()->user()?->settings?->country === $value ? ' ☑️' : '';
 //            $checked = auth()->user()?->load(['settings', 'chat'])->settings?->country === $value ? ' ☑️' : '';
 
