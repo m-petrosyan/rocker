c8373bc576a0a6dfd1f11854e55848adcd78268f
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-17 21:18:46 +0400

    fixing channel send

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -5,5 +5,15 @@ return [
     'change_city' => 'Thank you, now you will receive information about events from this location, to get the menu enter the command /events_list. &#10; To get the menu command /menu',
     'indicate_genre' => 'Now you can also specify the genre to get notified about new event.',
     'indicate_city' => 'Now you can also specify your city to receive new events only from your city.',
-    'add_event_instruction' => '<b>How to add event</b> &#10; &#10;Click on the "add event" button below or click menu -> menu -> add event, in the window that opens, fill in the required fields: country, poster, event type, title, content, genre, establishment name, date and time.  Click create.  After moderation it will be sent to all users (depending on user settings)',
+    'add_event_instruction' => '<b>How to Add an Event</b><br><br>Click the "Add Event" button below, or go to <b>Menu → Add Event</b>.<br><br>In the window that opens, fill in all required fields:<br><ul><li><b>Country</b></li>
+<li><b>Poster</b></li>
+<li><b>Event Type</b></li>
+<li><b>Title</b></li>
+<li><b>Content</b></li>
+<li><b>Genre</b></li>
+<li><b>Establishment Name</b></li>
+<li><b>Date and Time</b></li>
+</ul>
+Click <b>Create</b>.<br><br>
+After moderation, the event will be sent to all users according to their settings.',
 ];
