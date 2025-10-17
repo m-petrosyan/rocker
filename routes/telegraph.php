fbbe9b4569463df80b721e0ea0493a61fa3d8df1
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-17 15:36:18 +0400

    user create fixing

--- routes/telegraph.php
+++ routes/telegraph.php
@@ -2,13 +2,15 @@
 
 use App\Models\UserBot;
 
+\Illuminate\Support\Facades\Log::info('78787878asdasdasd');
+
 $userId = data_get(request()->input('callback_query'), 'from.id') ?? data_get(request()->input('message'), 'from.id');
-\Illuminate\Support\Facades\Log::info('auting878');
+\Illuminate\Support\Facades\Log::info('$userId', ['userId' => $userId]);
 if ($userId) {
     $userBot = UserBot::query()->where('chat_id', $userId)->first();
-    \Illuminate\Support\Facades\Log::info('auting878');
+    \Illuminate\Support\Facades\Log::info('$userBot', [$userBot]);
     if ($userBot) {
-        \Illuminate\Support\Facades\Log::info('auting879');
+        \Illuminate\Support\Facades\Log::info('$userBot', $userBot->user?->id);
         auth()->loginUsingId($userBot->user?->id);
     }
 }
