1852a6b312ee490ca718775fbaa879d6c3e01464
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-17 21:21:16 +0400

    change instruction

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -5,5 +5,5 @@ return [
     'change_city' => 'Thank you, now you will receive information about events from this location, to get the menu enter the command /events_list. &#10; To get the menu command /menu',
     'indicate_genre' => 'Now you can also specify the genre to get notified about new event.',
     'indicate_city' => 'Now you can also specify your city to receive new events only from your city.',
-    'add_event_instruction' => "<b>How to Add an Event</b>\n\nClick the 'Add Event' button below, or go to Menu → Add Event.\n\nFill in all required fields:\n- Country\n- Poster\n- Event Type\n- Title\n- Content\n- Genre\n- Establishment Name\n- Date and Time\n\nClick Create. After moderation, the event will be sent to users based on their settings.",
+    'add_event_instruction' => "<b>How to Add an Event</b>",
 ];
