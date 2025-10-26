b71e4d03b876b6a7d979f1d268d46db09283e6b5
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-26 20:50:49 +0400

    pagination fix

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -58,6 +58,6 @@ onMounted(() => {
   <PreloaderPwa v-if="isPWA" />
   <slot />
   <Footer v-if="!webApp" />
-  <Preloader v-if="webApp && !page" />
+  <Preloader v-if="webApp && !user" />
   <PwaNavbar v-if="isPWA || webApp" />
 </template>
