c8fc9ee206435f282ba627823d54547cc6225602
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-11-01 00:21:19 +0400

    fixing queries

--- resources/js/Components/Wrappers/GalleryWrapper.vue
+++ resources/js/Components/Wrappers/GalleryWrapper.vue
@@ -50,7 +50,6 @@ const deleteGallery = (id) => {
     });
   }
 };
-
 </script>
 
 <template>
--- resources/js/Pages/Band/Band.vue
+++ resources/js/Pages/Band/Band.vue
@@ -56,6 +56,7 @@ const url = fullUrl();
     <EventWrapper v-if="band.events.length" class="mt-20" :events="band.events" title="Upcoming events" />
     <!--    <GalleryWrapper v-if="band?.galleries.length" :galleries="band.galleries" title="Galleries" user />-->
     <BlogWrapper v-if="band.blogs.length" :blogs="band.blogs" class="mt-20" title="Articles about the group" />
+    <p>...</p>
   </GuestLayout>
 </template>
 
