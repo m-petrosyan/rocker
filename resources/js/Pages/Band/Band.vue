f989a194ac0e511d860809105230fbaf826d2e0d
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-10-31 18:55:44 +0400

    fixing queries

--- resources/js/Pages/Band/Band.vue
+++ resources/js/Pages/Band/Band.vue
@@ -7,6 +7,7 @@ import { fullUrl, getHostname } from '@/Helpers/urlHelper.js';
 import BandAlbums from '@/Components/Forms/BandAlbums.vue';
 import EventWrapper from '@/Components/Wrappers/EventWrapper.vue';
 import GalleryWrapper from '@/Components/Wrappers/GalleryWrapper.vue';
+import BlogWrapper from '@/Components/Wrappers/BlogWrapper.vue';
 
 defineProps({
   band: {
@@ -55,7 +56,7 @@ const url = fullUrl();
     <BandAlbums v-if="band?.albums" :albums="band.albums" />
     <EventWrapper v-if="band.events.length" class="mt-20" :events="band.events" title="Upcoming events" />
     <GalleryWrapper v-if="band?.galleries.length" :galleries="band.galleries" title="Galleries" user />
-    <!--    <BlogWrapper v-if="band.blogs.length" :blogs="band.blogs" class="mt-20" title="Articles about the group" />-->
+    <BlogWrapper v-if="band.blogs.length" :blogs="band.blogs" class="mt-20" title="Articles about the group" />
   </GuestLayout>
 </template>
 
