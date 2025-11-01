52d832b5cf37eb8caab511fb15866a87d066b066
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-11-02 00:38:52 +0400

    fixing 502

--- resources/js/Components/Preloader/Preloader.vue
+++ resources/js/Components/Preloader/Preloader.vue
@@ -4,15 +4,18 @@ import { onMounted, ref } from 'vue';
 import { router } from '@inertiajs/vue3';
 
 const visible = ref(false);
-
+const prevPath = ref(window.location.pathname);
 
 onMounted(() => {
   router.on('start', () => {
+    prevPath.value = window.location.pathname;
     visible.value = true;
   });
 
   router.on('finish', () => {
-    // window.scrollTo({ top: 0 });
+    if (window.location.pathname !== prevPath.value) {
+      window.scrollTo({ top: 0 });
+    }
     visible.value = false;
   });
 });
