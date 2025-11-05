8fd1a9d3a469e1a85b7d64c221927228806b075b
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-11-06 00:37:14 +0400

    testing

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -54,7 +54,7 @@ onMounted(() => {
     <meta name="author" :content="meta?.author ?? 'rocker.am'" />
     <link rel="canonical" :href="$page.props.canonical" />
   </Head>
-  <MainNavbar v-if="!isPWA && !webApp" />
+  <MainNavbar v-if="!(isPWA || webApp)" />
   <FleshNotification />
   <slot />
   <Footer v-if="!webApp" />
