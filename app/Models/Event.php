37af00c826fde2c810c595e595fa122d41c26e84
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-11-06 19:08:57 +0400

    fix tooltip

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -132,13 +132,11 @@ class Event extends Model implements Viewable, HasMedia
             ->quality(80)
             ->sharpen(7)
             ->optimize()
-            ->format('webp')
-            ->nonQueued();
+            ->format('webp');
 
         $this->addMediaConversion('large')
             ->width(1280)
             ->quality(90)
-            ->format('webp')
-            ->nonQueued();
+            ->format('webp');
     }
 }
