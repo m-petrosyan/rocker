b6e3b97bce908c1562c467e3edac09cab89566cf
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2025-11-15 15:44:48 +0400

    fix calendar google

--- resources/js/Components/Socials/AppleCalendar.vue
+++ resources/js/Components/Socials/AppleCalendar.vue
@@ -3,47 +3,47 @@ import { toIso8601WithEnd } from '@/Helpers/dateFormatHelper.js';
 import Icalcon from '@/Components/Icons/Icalcon.vue';
 
 const props = defineProps({
-    event: {
-        type: Object,
-        required: true
-    }
+  event: {
+    type: Object,
+    required: true
+  }
 });
 
-const date = toIso8601WithEnd('10.04.25', '15:00');
+const date = toIso8601WithEnd(props.event.start_date, props.event.start_time);
 
 const addToICalendar = () => {
-    const icsContent = [
-        'BEGIN:VCALENDAR',
-        'VERSION:2.0',
-        'PRODID:-//armcamp.com//EN',
-        'CALSCALE:GREGORIAN',
-        'BEGIN:VEVENT',
-        `DTSTART:${date.start}`,
-        `DTEND:${date.end}`,
-        `SUMMARY:${props.event.title}`,
-        `DESCRIPTION:${props.event.content}`,
-        `LOCATION:${props.event.location}`,
-        'END:VEVENT',
-        'END:VCALENDAR'
-    ].join('\r\n');
+  const icsContent = [
+    'BEGIN:VCALENDAR',
+    'VERSION:2.0',
+    'PRODID:-//armcamp.com//EN',
+    'CALSCALE:GREGORIAN',
+    'BEGIN:VEVENT',
+    `DTSTART:${date.start}`,
+    `DTEND:${date.end}`,
+    `SUMMARY:${props.event.title}`,
+    `DESCRIPTION:${props.event.content}`,
+    `LOCATION:${props.event.location}`,
+    'END:VEVENT',
+    'END:VCALENDAR'
+  ].join('\r\n');
 
-    const blob = new Blob([icsContent], { type: 'text/calendar;charset=utf-8' });
-    const url = URL.createObjectURL(blob);
+  const blob = new Blob([icsContent], { type: 'text/calendar;charset=utf-8' });
+  const url = URL.createObjectURL(blob);
 
-    const iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.src = url;
-    document.body.appendChild(iframe);
+  const iframe = document.createElement('iframe');
+  iframe.style.display = 'none';
+  iframe.src = url;
+  document.body.appendChild(iframe);
 
-    setTimeout(() => {
-        document.body.removeChild(iframe);
-        URL.revokeObjectURL(url);
-    }, 1000);
+  setTimeout(() => {
+    document.body.removeChild(iframe);
+    URL.revokeObjectURL(url);
+  }, 1000);
 };
 </script>
 
 <template>
-    <button @click="addToICalendar">
-        <Icalcon class="saturate-50 hover:saturate-100" />
-    </button>
+  <button @click="addToICalendar">
+    <Icalcon class="saturate-50 hover:saturate-100" />
+  </button>
 </template>
