eaf2c8222394d86e265251a4225015c1833b6dc3
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2026-01-22 22:01:19 +0400

    filters

--- resources/js/Components/Elements/EventFilters.vue
+++ resources/js/Components/Elements/EventFilters.vue
@@ -21,9 +21,9 @@ const props = defineProps({
 const emit = defineEmits(['close']);
 
 const dateRange = ref(
-    props.initialFilters.from && props.initialFilters.to
-        ? [new Date(props.initialFilters.from), new Date(props.initialFilters.to)]
-        : null
+  props.initialFilters.from && props.initialFilters.to
+    ? [new Date(props.initialFilters.from), new Date(props.initialFilters.to)]
+    : null
 );
 const selectedCountry = ref(props.initialFilters.country || 'all');
 
@@ -33,9 +33,9 @@ const applyFilters = () => {
     params.from = dateRange.value[0].toISOString().split('T')[0];
     params.to = dateRange.value[1].toISOString().split('T')[0];
   } else {
-      // If range is cleared or incomplete, we don't send from/to
+    // If range is cleared or incomplete, we don't send from/to
   }
-  
+
   if (selectedCountry.value && selectedCountry.value !== 'all') {
     params.country = selectedCountry.value;
   }
@@ -46,7 +46,7 @@ const applyFilters = () => {
 const debouncedApply = useDebounceFn(applyFilters, 600);
 
 watch([dateRange, selectedCountry], () => {
-    debouncedApply();
+  debouncedApply();
 });
 
 const resetFilters = () => {
@@ -66,19 +66,22 @@ const resetFilters = () => {
     leave-from-class="opacity-100 translate-y-0"
     leave-to-class="opacity-0 -translate-y-2"
   >
+
     <div v-if="show" class="mb-8 relative py-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
-        <div class="space-y-3">
-          <label class="block text-[9px] font-bold text-gray/40 uppercase tracking-[0.2em] ml-2">Select Date Range</label>
+      <div class="flex flex-col-reverse flex-col md:flex-row justify-center gap-8 items-start">
+        <div class="space-y-3 w-full md:w-auto text-center">
+          <label class="block text-[9px] font-bold text-gray/40 uppercase tracking-[0.2em] ml-2">Select Date
+            Range</label>
           <div class="p-2">
             <RangeDatePicker v-model="dateRange" />
           </div>
         </div>
-
-        <div class="flex flex-col justify-between h-full space-y-6">
+        <div v-if="auth?.user?.settings?.country === 'all'"
+             class="flex w-full md:w-auto flex-col justify-between h-full space-y-6">
           <div class="space-y-3">
-            <label class="block text-[9px] font-bold text-gray/40 uppercase tracking-[0.2em] ml-2">Filter by Country</label>
-            <div class="flex gap-x-4 ml-2">
+            <label class="block text-[9px] font-bold text-gray/40 uppercase tracking-[0.2em] ml-2 text-center">Filter by
+              Country</label>
+            <div class="flex justify-center gap-x-4 ml-2">
               <button
                 @click="selectedCountry = 'all'"
                 :class="[selectedCountry === 'all' ? 'text-red border-red' : 'text-gray/40 border-white/5 hover:text-white']"
@@ -102,22 +105,16 @@ const resetFilters = () => {
               </button>
             </div>
           </div>
-
-          <div class="flex items-center justify-between gap-x-4 pt-4 mt-auto">
-            <button
-              @click="resetFilters"
-              class="text-[9px] font-bold uppercase tracking-[0.2em] text-gray/30 hover:text-red transition-colors ml-2"
-            >
-              Reset All
-            </button>
-            
-            <div class="text-[9px] text-gray/20 uppercase tracking-widest italic ml-auto pr-2">
-              Changes apply automatically
-            </div>
-          </div>
         </div>
       </div>
-      <div class="mt-8 h-[1px] bg-gradient-to-r from-transparent via-white/5 to-transparent"></div>
+      <div class="flex justify-center items-center  gap-x-4 pt-4">
+        <button
+          @click="resetFilters"
+          class="text-[9px] font-bold uppercase tracking-[0.2em] text-gray/30 hover:text-red transition-colors ml-2"
+        >
+          Reset All
+        </button>
+      </div>
     </div>
   </transition>
 </template>
