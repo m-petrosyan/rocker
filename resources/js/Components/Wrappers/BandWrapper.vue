0ea5d94d053afdefca9f9464f0b1650dad1994aa
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2026-04-10 00:59:14 +0400

    fix edit band

--- resources/js/Components/Wrappers/BandWrapper.vue
+++ resources/js/Components/Wrappers/BandWrapper.vue
@@ -2,6 +2,10 @@
 import { computed, ref } from 'vue';
 import NavLink from '@/Components/NavLink.vue';
 import AddCard from '@/Components/Cards/AddCard.vue';
+import EyesIcon from '@/Components/Icons/EyesIcon.vue';
+import EditIcon from '@/Components/Icons/EditIcon.vue';
+import DeleteIcon from '@/Components/Icons/DeleteIcon.vue';
+import { router } from '@inertiajs/vue3';
 
 const props = defineProps({
     bands: {
@@ -14,6 +18,14 @@ const props = defineProps({
     genres: {
         type: Array,
         default: () => [],
+    },
+    owner: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
     },
     user: {
         type: Boolean,
@@ -28,6 +40,15 @@ const props = defineProps({
         default: false,
     },
 });
+
+const deleteBand = (id) => {
+    if (confirm('Are you sure you want to delete this band?')) {
+        router.delete(route('profile.bands.destroy', id), {
+            preserveState: false,
+            preserveScroll: true,
+        });
+    }
+};
 
 const bandsData = computed(() => {
     if (!props.bands) return [];
@@ -75,6 +96,39 @@ const onImgLoad = (e, id) => {
                                 :alt="band.name"
                                 @error="$event.target.src = band.logo.original"
                             />
+                        </div>
+                        <div
+                            v-if="owner || isAdmin"
+                            class="absolute inset-0 z-10 flex flex-col justify-between rounded-xl bg-blackTransparent2 p-1 md:opacity-100"
+                        >
+                            <div
+                                @click.prevent
+                                class="flex justify-end gap-y-2"
+                            >
+                                <div
+                                    tooltip="Views"
+                                    class="flex cursor-default items-center gap-x-2 rounded px-2 py-1"
+                                >
+                                    <EyesIcon />
+                                    {{ band.views_count }}
+                                </div>
+                            </div>
+                            <div class="flex justify-between">
+                                <NavLink
+                                    tooltip="Edit"
+                                    :href="route('profile.bands.edit', band.id)"
+                                    class="rounded bg-black/50 p-1 transition hover:bg-black/80"
+                                >
+                                    <EditIcon />
+                                </NavLink>
+                                <button
+                                    tooltip="Delete"
+                                    @click.prevent="deleteBand(band.id)"
+                                    class="text-red-500 hover:text-red-700 rounded bg-black/50 p-1 transition"
+                                >
+                                    <DeleteIcon />
+                                </button>
+                            </div>
                         </div>
                     </div>
                     <div class="mt-2 p-2 text-center">
