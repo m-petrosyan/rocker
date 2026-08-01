3b55368d29d3611c48125ec1bf7afe57d8d076b7
Miqayel Petrosyan <miqayelpetrosyan@gmail.com>
2026-08-01 12:40:58 +0400

    horizon fix

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user = null) {
             return in_array(optional($user)->email, [
-                'miqayelpetrosyan@email.com',
+                'miqayelpetrosyan@gmail.com',
             ]);
         });
     }
